1e1b0ff4425c0287ab174019b5ea18fa919c2559
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-05-27 09:17:33 +0530

    Add posts app

---
+++ posts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    # Category options matching your sketch
+    CATEGORY_CHOICES = [
+        ('Articles', 'Articles'),
+        ('Poems', 'Poems'),
+        ('Stories', 'Stories'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField(max_length=1500) 
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    
+    category = models.CharField(
+        max_length=20, 
+        choices=CATEGORY_CHOICES, 
+        default='Articles'
+    )
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self): 
+        return f'{self.user.username} - {self.text[:20]}...'
