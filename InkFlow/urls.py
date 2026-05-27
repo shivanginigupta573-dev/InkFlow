b62f62a8bb0df5a225b0e62b320ca0928829028d
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-05-27 09:15:39 +0530

    Configure Django project

---
+++ InkFlow/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    
+    # Connects your 'posts' app URLs
+    path('', include('posts.urls')),
+    
+    path("__reload__/", include("django_browser_reload.urls")),
+
+    # Connects Django's built-in login/logout system
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+# This allows the browser to see the photos you upload in VS Code
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
